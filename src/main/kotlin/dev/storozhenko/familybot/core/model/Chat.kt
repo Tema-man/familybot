9cd817cbc3545480bd7af9a360e5e407fe205f2d
Artemii Vishnevskii <temaa.mann@gmail.com>
2023-04-13 13:55:40 +0200

    add chat key

--- src/main/kotlin/dev/storozhenko/familybot/core/model/Chat.kt
+++ src/main/kotlin/dev/storozhenko/familybot/core/model/Chat.kt
@@ -1,8 +1,12 @@
 package dev.storozhenko.familybot.core.model
 
+import dev.storozhenko.familybot.core.services.settings.ChatEasyKey
+
 data class Chat(
     val id: Long,
     val name: String?,
     val idString: String = id.toString(),
     val isGroup: Boolean = false,
-)
+) {
+    val key = ChatEasyKey(id)
+}
