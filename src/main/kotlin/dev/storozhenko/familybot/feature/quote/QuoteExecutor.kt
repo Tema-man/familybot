3ff890ce620dfe6538bcae3bae48b2fd5d06be7f
Artemii Vishnevskii <temaa.mann@gmail.com>
2023-04-13 16:38:53 +0200

    convert huificator and quote executors to intent

--- src/main/kotlin/dev/storozhenko/familybot/feature/quote/QuoteExecutor.kt
+++ src/main/kotlin/dev/storozhenko/familybot/feature/quote/QuoteExecutor.kt
@@ -1,22 +1,22 @@
 package dev.storozhenko.familybot.feature.quote
 
-import dev.storozhenko.familybot.core.executor.CommandExecutor
+import dev.storozhenko.familybot.core.executor.IntentExecutor
 import dev.storozhenko.familybot.core.model.Command
 import dev.storozhenko.familybot.core.model.action.Action
 import dev.storozhenko.familybot.core.model.action.SendTextAction
-import dev.storozhenko.familybot.core.services.router.model.ExecutorContext
+import dev.storozhenko.familybot.core.model.intent.CommandIntent
+import dev.storozhenko.familybot.core.model.intent.Intent
+import dev.storozhenko.familybot.core.services.router.model.Priority
 import org.springframework.stereotype.Component
-import org.telegram.telegrambots.meta.bots.AbsSender
 
 @Component
-class QuoteExecutor(private val quoteRepository: QuoteRepository) : CommandExecutor() {
+class QuoteExecutor(private val quoteRepository: QuoteRepository) : IntentExecutor {
 
-    override fun command() = Command.QUOTE
+    val command = Command.QUOTE
 
-    override fun execute(context: ExecutorContext): suspend (AbsSender) -> Action? = {
-        SendTextAction(
-            text = quoteRepository.getRandom(),
-            context = context
-        )
-    }
+    override val priority: Priority = Priority.MEDIUM
+
+    override fun canExecute(intent: Intent): Boolean = (intent as? CommandIntent)?.command == command
+
+    override fun execute(intent: Intent): Action = SendTextAction(text = quoteRepository.getRandom(), chat = intent.chat)
 }
