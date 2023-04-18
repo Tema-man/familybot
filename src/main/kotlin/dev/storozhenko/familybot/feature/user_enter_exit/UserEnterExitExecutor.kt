d7838c502c0d83455e9fea77b520f065d96e2ac8
Vishnevskii Artemii <artemii.vishnevskii@revolut.com>
2023-04-19 00:59:44 +0200

    refactor UserEnterExitExecutor to use Intents and Actions

--- src/main/kotlin/dev/storozhenko/familybot/feature/user_enter_exit/UserEnterExitExecutor.kt
+++ src/main/kotlin/dev/storozhenko/familybot/feature/user_enter_exit/UserEnterExitExecutor.kt
@@ -1,51 +1,48 @@
 package dev.storozhenko.familybot.feature.user_enter_exit
 
-import dev.storozhenko.familybot.telegram.send
+import dev.storozhenko.familybot.core.bot.BotConfig
 import dev.storozhenko.familybot.core.executor.Configurable
-import dev.storozhenko.familybot.core.executor.Executor
+import dev.storozhenko.familybot.core.executor.IntentExecutor
+import dev.storozhenko.familybot.core.model.action.Action
+import dev.storozhenko.familybot.core.model.action.SendTextAction
+import dev.storozhenko.familybot.core.model.intent.BotAddedToChatIntent
+import dev.storozhenko.familybot.core.model.intent.Intent
+import dev.storozhenko.familybot.core.model.intent.UserJoinedIntent
+import dev.storozhenko.familybot.core.model.intent.UserLeftIntent
 import dev.storozhenko.familybot.core.services.router.model.ExecutorContext
 import dev.storozhenko.familybot.core.services.router.model.FunctionId
 import dev.storozhenko.familybot.core.services.router.model.Priority
+import dev.storozhenko.familybot.core.services.talking.Dictionary
 import dev.storozhenko.familybot.core.services.talking.model.Phrase
-import dev.storozhenko.familybot.core.bot.BotConfig
-import dev.storozhenko.familybot.core.model.action.Action
 import org.springframework.stereotype.Component
-import org.telegram.telegrambots.meta.api.objects.Message as TelegramMessage
-import org.telegram.telegrambots.meta.bots.AbsSender
 
 @Component
 class UserEnterExitExecutor(
-    private val botConfig: BotConfig
-) : Executor, Configurable {
+    private val botConfig: BotConfig,
+    private val dictionary: Dictionary
+) : IntentExecutor, Configurable {
 
     override fun getFunctionId(context: ExecutorContext) = FunctionId.GREETINGS
 
-    override fun priority(context: ExecutorContext) = Priority.LOW
+    override val priority: Priority = Priority.LOW
 
-    override fun execute(context: ExecutorContext): suspend (AbsSender) -> Action? {
-        val message = context.message
-        val phrase = when {
-            isUserLeft(message) -> Phrase.USER_LEAVING_CHAT
-            isNewChat(message) -> Phrase.BOT_WELCOME_MESSAGE
-            else -> Phrase.USER_ENTERING_CHAT
+    override fun execute(intent: Intent): Action? {
+        val phrase = when (intent) {
+            is UserLeftIntent -> Phrase.USER_LEAVING_CHAT
+            is UserJoinedIntent -> Phrase.USER_ENTERING_CHAT
+            is BotAddedToChatIntent -> Phrase.BOT_WELCOME_MESSAGE
+            else -> return null
         }
-        return {
-            it.send(
-                context,
-                context.phrase(phrase),
-                replyToUpdate = true,
-                shouldTypeBeforeSend = true
-            )
-            null
-        }
+
+        return SendTextAction(
+            text = dictionary.get(phrase, intent.chat.key),
+            chat = intent.chat,
+            asReplyToIntentId = intent.id
+        )
     }
 
-    override fun canExecute(context: ExecutorContext) =
-        isUserLeft(context.message) || isUserEntered(context.message)
-
-    private fun isUserLeft(message: TelegramMessage) = message.leftChatMember != null
-    private fun isUserEntered(message: TelegramMessage) = message.newChatMembers?.isNotEmpty() ?: false
-    private fun isNewChat(message: TelegramMessage) =
-        message.newChatMembers.any { it.isBot && it.userName == botConfig.botName }
+    override fun canExecute(intent: Intent): Boolean = intent is UserJoinedIntent
+        || intent is UserLeftIntent
+        || intent is BotAddedToChatIntent
 
 }
